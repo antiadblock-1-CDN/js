089aeae63275b4a6aa85ad5ee212635a2e8e87be
antiadblock-1-CDN <122008752+antiadblock-1-CDN@users.noreply.github.com>
2023-01-18 09:30:50 +0100

    Automated update

---
+++ vosveteitsk-v1.js
@@ -0,0 +1,145 @@
+/* Version: A_01 - 18.01. 2023 - 09:30:49 */
+
+
+var imms = document.createElement("script");
+imms.src = "https://cdn.jsdelivr.net/gh/impression-media/js/impublishertag.repeater.log.js";
+imms.type = "text/javascript";
+imms.async = true;
+document.getElementsByTagName("head")[0].appendChild(imms);
+
+window.Impressionmedia = window.Impressionmedia || {};
+window.Impressionmedia.events = window.Impressionmedia.events || [];
+
+ 
+//targeting definitions
+
+//setTimeout(function(){
+
+var div = document.createElement("div");
+div.setAttribute("id", "crt-1406721-vosveteitsk");
+div.setAttribute("style", "height:210px; width:750px; margin:0px -20px;");
+document.querySelector("#adinserter-1").insertAdjacentElement("afterend", div);
+    
+//setTimeout(function(){
+console.log("call Impressionmedia push");
+Impressionmedia.events.push(function() {Impressionmedia.DisplayAcceptableAdIfAdblocked({"zoneid": 1406721,"containerid": "crt-1406721-vosveteitsk","overrideZoneFloor": false,"callbacksuccess": function(){console.log("antiAdBlock callbacksuccess");
+        
+        
+        
+        
+        
+        
+        
+                
+            
+            
+            
+    ;},"callIfNotAdBlocked":function(){console.log("antiAdBlock callIfNotAdBlocked")}, });});;
+//}, 5000);
+
+
+var div = document.createElement("div");
+div.setAttribute("id", "crt-1406726-vosveteitsk");
+div.setAttribute("style", "height:970px; width:250px; margin:10px auto;");
+document.querySelector("#footer").insertAdjacentElement("beforebegin", div);
+    
+//setTimeout(function(){
+console.log("call Impressionmedia push");
+Impressionmedia.events.push(function() {Impressionmedia.DisplayAcceptableAdIfAdblocked({"zoneid": 1406726,"containerid": "crt-1406726-vosveteitsk","overrideZoneFloor": false,"callbacksuccess": function(){console.log("antiAdBlock callbacksuccess");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+                
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    ;},"callIfNotAdBlocked":function(){console.log("antiAdBlock callIfNotAdBlocked")}, });});;
+//}, 5000);
+
+
+var div = document.createElement("div");
+div.setAttribute("id", "crt-1403239-vosveteitsk");
+div.setAttribute("style", "height:600px; width:300px; margin:10px auto;");
+document.querySelector(".sidebar").insertAdjacentElement("beforeend", div);
+    
+//setTimeout(function(){
+console.log("call Impressionmedia push");
+Impressionmedia.events.push(function() {Impressionmedia.DisplayAcceptableAdIfAdblocked({"zoneid": 1403239,"containerid": "crt-1403239-vosveteitsk","overrideZoneFloor": false,"callbacksuccess": function(){console.log("antiAdBlock callbacksuccess");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+                
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    ;},"callIfNotAdBlocked":function(){console.log("antiAdBlock callIfNotAdBlocked")}, });});;
+//}, 5000);
+
+
+//}, 100);
+
