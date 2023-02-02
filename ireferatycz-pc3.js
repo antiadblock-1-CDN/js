11be6bf2bd7ee684326ccd7624dff9cf4d728a6a
antiadblock-1-CDN <122008752+antiadblock-1-CDN@users.noreply.github.com>
2023-02-02 09:51:54 +0100

    Automated update

--- ireferatycz-pc3.js
+++ ireferatycz-pc3.js
@@ -1,4 +1,4 @@
-/* Version: A_01 - 02.02. 2023 - 09:43:41 */
+/* Version: A_01 - 02.02. 2023 - 09:51:52 */
 
 
 var imms = document.createElement("script");
@@ -15,42 +15,12 @@ window.Impressionmedia.events = window.Impressionmedia.events || [];
 
 //setTimeout(function(){
 
-var selection = document.querySelector("#hlavicka") !== null;
-if (selection) {
-var div = document.createElement("div");
-div.setAttribute("id", "crt-1406721-ireferatycz");
-div.setAttribute("style", "height: 210px; width: 760px;margin: auto;");
-document.querySelector("#hlavicka").insertAdjacentElement("afterend", div);
-    
-//setTimeout(function(){
-console.log("call Impressionmedia push");
-Impressionmedia.events.push(function() {Impressionmedia.DisplayAcceptableAdIfAdblocked({"zoneid": 1406721,"containerid": "crt-1406721-ireferatycz","overrideZoneFloor": false,"callbacksuccess": function(){console.log("antiAdBlock callbacksuccess");
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        document.querySelector('#crt-1406721-ireferatycz').style.height = "210px";
-            
-            
-            
-            
-            
-    ;},"callIfNotAdBlocked":function(){console.log("antiAdBlock callIfNotAdBlocked")}, });});;
-//}, 5000);
-}
-
-var selection = document.querySelector("#obsahClanekMinHeight") !== null;
+var selection = document.querySelector("#main") !== null;
 if (selection) {
 var div = document.createElement("div");
 div.setAttribute("id", "crt-1406726-ireferatycz");
-div.setAttribute("style", "height: 210px; width: 980px; margin: auto;");
-document.querySelector("#obsahClanekMinHeight").insertAdjacentElement("afterend", div);
+div.setAttribute("style", "height: 210px; width: 980px; margin: auto; text-align:center");
+document.querySelector("#main").insertAdjacentElement("beforebegin", div);
     
 //setTimeout(function(){
 console.log("call Impressionmedia push");
@@ -75,7 +45,13 @@ Impressionmedia.events.push(function() {Impressionmedia.DisplayAcceptableAdIfAdb
         
         
         
+        
+        
+        
+        
         document.querySelector('#crt-1406726-ireferatycz').style.height = "260px";
+            
+            
             
             
             
