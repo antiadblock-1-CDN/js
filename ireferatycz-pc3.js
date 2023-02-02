039c9d4b4bd982cf633397fdb69487e786da0b36
antiadblock-1-CDN <122008752+antiadblock-1-CDN@users.noreply.github.com>
2023-02-02 09:56:24 +0100

    Automated update

--- ireferatycz-pc3.js
+++ ireferatycz-pc3.js
@@ -1,4 +1,4 @@
-/* Version: A_01 - 02.02. 2023 - 09:51:52 */
+/* Version: A_01 - 02.02. 2023 - 09:56:23 */
 
 
 var imms = document.createElement("script");
@@ -14,6 +14,48 @@ window.Impressionmedia.events = window.Impressionmedia.events || [];
 //targeting definitions
 
 //setTimeout(function(){
+
+var selection = document.querySelector("#obsahClanekMinHeight") !== null;
+if (selection) {
+var div = document.createElement("div");
+div.setAttribute("id", "crt-1406721-ireferatycz");
+div.setAttribute("style", "height: 210px; width: 760px;margin: auto; text-align:center");
+document.querySelector("#obsahClanekMinHeight").insertAdjacentElement("beforeend", div);
+    
+//setTimeout(function(){
+console.log("call Impressionmedia push");
+Impressionmedia.events.push(function() {Impressionmedia.DisplayAcceptableAdIfAdblocked({"zoneid": 1406721,"containerid": "crt-1406721-ireferatycz","overrideZoneFloor": false,"callbacksuccess": function(){console.log("antiAdBlock callbacksuccess");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        document.querySelector('#crt-1406721-ireferatycz').style.height = "210px";
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    ;},"callIfNotAdBlocked":function(){console.log("antiAdBlock callIfNotAdBlocked")}, });});;
+//}, 5000);
+}
 
 var selection = document.querySelector("#main") !== null;
 if (selection) {
