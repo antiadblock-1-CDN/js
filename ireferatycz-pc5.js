7946d9eb215b6cda3ddd34a8b2968ce46acc6171
antiadblock-1-CDN <122008752+antiadblock-1-CDN@users.noreply.github.com>
2023-02-02 15:31:35 +0100

    Automated update

--- ireferatycz-pc5.js
+++ ireferatycz-pc5.js
@@ -1,4 +1,4 @@
-/* Version: A_01 - 02.02. 2023 - 15:22:26 */
+/* Version: A_01 - 02.02. 2023 - 15:31:33 */
 
 
 var imms = document.createElement("script");
@@ -15,43 +15,19 @@ window.Impressionmedia.events = window.Impressionmedia.events || [];
 
 //setTimeout(function(){
 
-var selection = document.querySelector("#obsahClanekMinHeight") !== null;
-if (selection) {
-var div = document.createElement("div");
-div.setAttribute("id", "crt-1406721-ireferatycz");
-div.setAttribute("style", "height: 210px; width: 760px;margin: auto; text-align:center");
-document.querySelector("#obsahClanekMinHeight").insertAdjacentElement("beforeend", div);
-    
-//setTimeout(function(){
-console.log("call Impressionmedia push");
-Impressionmedia.events.push(function() {Impressionmedia.DisplayAcceptableAdIfAdblocked({"zoneid": 1406721,"containerid": "crt-1406721-ireferatycz","overrideZoneFloor": false,"callbacksuccess": function(){console.log("antiAdBlock callbacksuccess");
-        
-        
-        document.querySelector('#crt-1406721-ireferatycz').style.height = "210px";
-            
-    ;},"callIfNotAdBlocked":function(){console.log("antiAdBlock callIfNotAdBlocked")}, });});;
-//}, 5000);
-}
-
 var selection = document.querySelector("#hlavicka") !== null;
 if (selection) {
 var div = document.createElement("div");
-div.setAttribute("id", "crt-1403238-ireferatycz");
-div.setAttribute("style", "height: 260px; width: 980px; margin: auto; text-align:center");
+div.setAttribute("id", "crt-1406726-ireferatycz");
+div.setAttribute("style", "height: auto; width: 980px; text-align:center");
 document.querySelector("#hlavicka").insertAdjacentElement("afterend", div);
     
 //setTimeout(function(){
 console.log("call Impressionmedia push");
-Impressionmedia.events.push(function() {Impressionmedia.DisplayAcceptableAdIfAdblocked({"zoneid": 1403238,"containerid": "crt-1403238-ireferatycz","overrideZoneFloor": false,"callbacksuccess": function(){console.log("antiAdBlock callbacksuccess");
+Impressionmedia.events.push(function() {Impressionmedia.DisplayAcceptableAdIfAdblocked({"zoneid": 1406726,"containerid": "crt-1406726-ireferatycz","overrideZoneFloor": false,"callbacksuccess": function(){console.log("antiAdBlock callbacksuccess");
         
         
         
-        
-        
-        
-        document.querySelector('#crt-1403238-ireferatycz').style.height = "260px";
-            
-            
             
     ;},"callIfNotAdBlocked":function(){console.log("antiAdBlock callIfNotAdBlocked")}, });});;
 //}, 5000);
